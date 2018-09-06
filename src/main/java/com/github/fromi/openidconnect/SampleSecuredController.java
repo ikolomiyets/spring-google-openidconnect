19a13460d47b2fbf1a759275cb10d3831d9eb8be
Igor.Kolomiyets <igor.kolomiyets@mdsglobal.com>
2018-09-06 16:38:43 +0100

    Implemented logout

--- src/main/java/com/github/fromi/openidconnect/SampleSecuredController.java
+++ src/main/java/com/github/fromi/openidconnect/SampleSecuredController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller
 public class SampleSecuredController {
@@ -25,12 +28,23 @@ public class SampleSecuredController {
     @Value("${oauth2.redirectUri}")
     private String redirectUri;
 
+    @Value("${oauth2.logoutUri}")
+    private String logoutUri;
+
+    @Value("${oauth2.baseUri}")
+    private String baseUri;
+
     @RequestMapping("/test")
     public String test(Model model) {
         UserInfo userInfo = (UserInfo) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("userId", userInfo.getName());
         model.addAttribute("checkSessionUri", checkSessionUri + "?client_id=" + clientId);
         return "test";
+    }
+
+    @RequestMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.sendRedirect(String.format("%s?post_logout_redirect_uri=%s", logoutUri, baseUri));
     }
 
     @RequestMapping("/admin")
--- src/main/java/com/github/fromi/openidconnect/security/SecurityConfiguration.java
+++ src/main/java/com/github/fromi/openidconnect/security/SecurityConfiguration.java
@@ -1,15 +1,22 @@
 package com.github.fromi.openidconnect.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.web.context.NullSecurityContextRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import static org.springframework.http.HttpMethod.GET;
 
@@ -18,6 +25,9 @@ import static org.springframework.http.HttpMethod.GET;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final String LOGIN_URL = "/login";
+
+    @Value("${oauth2.logoutUri}")
+    private String logoutUri;
 
     @Autowired
     private OAuth2ClientContextFilter oAuth2ClientContextFilter;
@@ -35,12 +45,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .addFilterAfter(oAuth2ClientContextFilter, AbstractPreAuthenticatedProcessingFilter.class)
-                .addFilterAfter(openIdConnectAuthenticationFilter(), OAuth2ClientContextFilter.class)
-                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint())
-                .and().authorizeRequests()
+            .addFilterAfter(oAuth2ClientContextFilter, AbstractPreAuthenticatedProcessingFilter.class)
+            .addFilterAfter(openIdConnectAuthenticationFilter(), OAuth2ClientContextFilter.class)
+            .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint())
+            .and()
+                .authorizeRequests()
                 .antMatchers(GET, "/", LOGIN_URL, "/rest/**").permitAll()
                 .antMatchers(GET, "/test", "/admin").hasRole("OPERATOR")
-                .antMatchers(GET, "/admin").hasRole("ADMINISTRATOR");
+                .antMatchers(GET, "/admin").hasRole("ADMINISTRATOR")
+            .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .logoutUrl(logoutUri);
     }
 }
